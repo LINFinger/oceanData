3e73238f145880586105884ff328df2b2d1b764e
LINFinger <794518882@qq.com>
2021-01-17 15:26:38 +0800

    multithreading it

--- oceandata.py
+++ oceandata.py
@@ -2,6 +2,8 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QFileDialog
 from PyQt5.QtGui import QIcon
+from threading import Thread
+from datetime import datetime
 
 import processing
 
@@ -13,6 +15,8 @@ class OceanDataProcess:
         self.ui.clearButton.clicked.connect(self.clearConsole)
         self.ui.pathButton.clicked.connect(self.getFilePath)
         self.ui.outputButton.clicked.connect(self.outputFile)
+        self.consoleOutputInfoIn = ''
+        self.consoleOutputInfoOut = ''
 
     def clearConsole(self):
         self.ui.console.clear()
@@ -22,17 +26,24 @@ class OceanDataProcess:
         self.ui.pathEdit.setText(file_path)
 
     def outputFile(self):
-        self.ui.console.append(
-            '==================================start==================================')
         file_path = self.ui.pathEdit.text()
         file_type = self.ui.typeComboBox.currentText()
-        self.ui.console.append(f'har文件路径 : {file_path}\n\n导出文件格式 : {file_type}')
-        self.ui.console.append('\n--------------------------------------------------------------------------\n')
+        self.consoleOutputInfoIn = ('===========================start==========================\n' +
+                                    f'{datetime.now().strftime("%Y-%m-%d ** %H:%M:%S")}\n\n' +
+                                    f'har文件路径 : {file_path}\n\n导出文件格式 : {file_type}\n' +
+                                    '\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n'
+                                    )
+        self.ui.console.append(self.consoleOutputInfoIn)
         od = processing.OceanData(file_path, file_type)
+        thread = Thread(target=self.processThread, args=[od])
+        thread.start()
+
+    def processThread(self, od):
         od.dataProcessing()
-        self.ui.console.append(od.getOutput())
-        self.ui.console.append(
-            '===================================end===================================\n')
+        self.consoleOutputInfoOut = (od.getOutput() +
+                                     '\n===========================end===========================\n'
+                                     )
+        self.ui.console.append(self.consoleOutputInfoOut)
         self.ui.console.ensureCursorVisible()
 
 
